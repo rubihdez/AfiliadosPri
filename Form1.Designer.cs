1888cbeb2659208164b8abb1564e7e04f3b7ce5d
Rubihdez <alu.24130045@correo.itlalaguna.edu.mx>
2025-09-30 15:10:35 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace AfiliadosPri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.cmbBoxMunicipio = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNombreArchivo = new System.Windows.Forms.TextBox();
+            this.btnResetear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumeroAfiliaciones = new System.Windows.Forms.TextBox();
+            this.checkBoxFecha = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dtpFechaFin = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaInicio = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ESTADO:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstado.Location = new System.Drawing.Point(122, 298);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(88, 25);
+            this.txtEstado.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ARCHIVO:";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCargar.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(401, 9);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(117, 33);
+            this.btnCargar.TabIndex = 3;
+            this.btnCargar.Text = "CARGAR";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click_1);
+            // 
+            // cmbBoxMunicipio
+            // 
+            this.cmbBoxMunicipio.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBoxMunicipio.FormattingEnabled = true;
+            this.cmbBoxMunicipio.Items.AddRange(new object[] {
+            "",
+            "Aguascalientes",
+            "",
+            "",
+            "Asientos",
+            "",
+            "",
+            "Calvillo",
+            "",
+            "",
+            "Cosío",
+            "",
+            "",
+            "Jesús María",
+            "",
+            "",
+            "El Llano",
+            "",
+            "",
+            "Pabellón de Arteaga",
+            "",
+            "",
+            "Rincón de Romos",
+            "",
+            "",
+            "San Francisco de los Romo",
+            "",
+            "",
+            "San José de Gracia",
+            "",
+            "",
+            "Tepezalá",
+            "",
+            "",
+            "Baja California",
+            "Ensenada",
+            "",
+            "",
+            "Mexicali",
+            "",
+            "",
+            "Playas de Rosarito",
+            "",
+            "",
+            "Tecate",
+            "",
+            "",
+            "Tijuana",
+            "Comondú",
+            "",
+            "",
+            "La Paz",
+            "",
+            "",
+            "Los Cabos",
+            "",
+            "",
+            "Loreto",
+            "",
+            "",
+            "Mulegé",
+            "Campeche",
+            "",
+            "",
+            "Calkiní",
+            "",
+            "",
+            "Carmen",
+            "",
+            "",
+            "Champotón",
+            "",
+            "",
+            "Escárcega",
+            "",
+            "",
+            "Hecelchakán",
+            "",
+            "",
+            "Hopelchén",
+            "",
+            "",
+            "Palizada",
+            "",
+            "",
+            "Tenabo",
+            "",
+            "",
+            "Candelaria",
+            "",
+            "",
+            "Calkiní",
+            "",
+            "",
+            "Carmen",
+            "",
+            "",
+            "Champotón"});
+            this.cmbBoxMunicipio.Location = new System.Drawing.Point(122, 366);
+            this.cmbBoxMunicipio.Name = "cmbBoxMunicipio";
+            this.cmbBoxMunicipio.Size = new System.Drawing.Size(106, 26);
+            this.cmbBoxMunicipio.TabIndex = 4;
+            this.cmbBoxMunicipio.SelectedIndexChanged += new System.EventHandler(this.cmbBoxMunicipio_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "MUNICIPIO:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(693, 217);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // txtNombreArchivo
+            // 
+            this.txtNombreArchivo.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreArchivo.Location = new System.Drawing.Point(162, 12);
+            this.txtNombreArchivo.Name = "txtNombreArchivo";
+            this.txtNombreArchivo.Size = new System.Drawing.Size(220, 31);
+            this.txtNombreArchivo.TabIndex = 8;
+            // 
+            // btnResetear
+            // 
+            this.btnResetear.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnResetear.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnResetear.Location = new System.Drawing.Point(524, 9);
+            this.btnResetear.Name = "btnResetear";
+            this.btnResetear.Size = new System.Drawing.Size(117, 33);
+            this.btnResetear.TabIndex = 9;
+            this.btnResetear.Text = "RESET";
+            this.btnResetear.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(269, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "NUMERO DE AFILIACIONES:";
+            // 
+            // txtNumeroAfiliaciones
+            // 
+            this.txtNumeroAfiliaciones.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroAfiliaciones.Location = new System.Drawing.Point(476, 293);
+            this.txtNumeroAfiliaciones.Name = "txtNumeroAfiliaciones";
+            this.txtNumeroAfiliaciones.Size = new System.Drawing.Size(100, 27);
+            this.txtNumeroAfiliaciones.TabIndex = 11;
+            // 
+            // checkBoxFecha
+            // 
+            this.checkBoxFecha.AutoSize = true;
+            this.checkBoxFecha.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxFecha.Location = new System.Drawing.Point(385, 417);
+            this.checkBoxFecha.Name = "checkBoxFecha";
+            this.checkBoxFecha.Size = new System.Drawing.Size(95, 26);
+            this.checkBoxFecha.TabIndex = 12;
+            this.checkBoxFecha.Text = "FECHA:";
+            this.checkBoxFecha.UseVisualStyleBackColor = true;
+            this.checkBoxFecha.CheckedChanged += new System.EventHandler(this.checkBoxFecha_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(486, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "FECHA INICIAL:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(486, 461);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 22);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "FECHA FINAL:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AfiliadosPri.Properties.Resources.priii;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dtpFechaFin
+            // 
+            this.dtpFechaFin.Location = new System.Drawing.Point(607, 456);
+            this.dtpFechaFin.Name = "dtpFechaFin";
+            this.dtpFechaFin.Size = new System.Drawing.Size(200, 27);
+            this.dtpFechaFin.TabIndex = 22;
+            this.dtpFechaFin.ValueChanged += new System.EventHandler(this.dtpFechaFin_ValueChanged_1);
+            // 
+            // dtpFechaInicio
+            // 
+            this.dtpFechaInicio.Location = new System.Drawing.Point(602, 418);
+            this.dtpFechaInicio.Name = "dtpFechaInicio";
+            this.dtpFechaInicio.Size = new System.Drawing.Size(200, 27);
+            this.dtpFechaInicio.TabIndex = 23;
+            this.dtpFechaInicio.ValueChanged += new System.EventHandler(this.dtpFechaInicio_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(834, 511);
+            this.Controls.Add(this.dtpFechaInicio);
+            this.Controls.Add(this.dtpFechaFin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBoxFecha);
+            this.Controls.Add(this.txtNumeroAfiliaciones);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnResetear);
+            this.Controls.Add(this.txtNombreArchivo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbBoxMunicipio);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MIEMBROS AFILIADOS PRI";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ComboBox cmbBoxMunicipio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNombreArchivo;
+        private System.Windows.Forms.Button btnResetear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumeroAfiliaciones;
+        private System.Windows.Forms.CheckBox checkBoxFecha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtpFechaFin;
+        private System.Windows.Forms.DateTimePicker dtpFechaInicio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AfiliadosPri
+{
+    public partial class Form1 : Form
+    {
+        private DataTable datosExcel = new DataTable();
+        private DataView datosFiltrados;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Formato de fechas    
+            dtpFechaInicio.Format = DateTimePickerFormat.Custom;
+            dtpFechaInicio.CustomFormat = "dd/MM/yyyy";
+            dtpFechaFin.Format = DateTimePickerFormat.Custom;
+            dtpFechaFin.CustomFormat = "dd/MM/yyyy";
+
+            dtpFechaInicio.Enabled = false;
+            dtpFechaFin.Enabled = false;
+        }
+
+        private async void btnCargar_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Excel|*.xlsx;*.xls" };
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            string rutaArchivo = ofd.FileName;
+            txtNombreArchivo.Text = Path.GetFileName(rutaArchivo);
+
+            btnCargar.Enabled = false;
+            btnCargar.Text = "Cargando...";
+
+            try
+            {
+                var filas = new System.Collections.Generic.List<DataRow>();
+
+                await System.Threading.Tasks.Task.Run(() =>
+                {
+                    using (var stream = new FileStream(rutaArchivo, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (var workbook = new XLWorkbook(stream))
+                    {
+                        var worksheet = workbook.Worksheet(1);
+
+                        datosExcel.Clear();
+                        datosExcel.Columns.Clear();
+
+                        var headerRow = worksheet.Row(1);
+                        foreach (var cell in headerRow.CellsUsed())
+                        {
+                            string colName = cell.GetValue<string>().Trim();
+                            if (!datosExcel.Columns.Contains(colName))
+                            {
+                                if (colName.Equals("FechaAfiliacion", StringComparison.OrdinalIgnoreCase))
+                                    datosExcel.Columns.Add(colName, typeof(DateTime));
+                                else
+                                    datosExcel.Columns.Add(colName);
+                            }
+                            // Optionally, handle duplicates here (e.g., log, rename, or notify)
+                        }
+
+                        foreach (var row in worksheet.RowsUsed().Skip(1))
+                        {
+                            var dr = datosExcel.NewRow();
+                            for (int i = 0; i < datosExcel.Columns.Count; i++)
+                            {
+                                string valor = row.Cell(i + 1).GetValue<string>().Trim();
+                                if (datosExcel.Columns[i].ColumnName.Equals("FechaAfiliacion", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    if (DateTime.TryParse(valor, out DateTime fecha))
+                                        dr[i] = fecha;
+                                    else
+                                        dr[i] = DBNull.Value;
+                                }
+                                else
+                                {
+                                    dr[i] = valor;
+                                }
+                            }
+                            filas.Add(dr);
+                        }
+                    }
+                });
+
+                foreach (var dr in filas)
+                    datosExcel.Rows.Add(dr);
+
+                datosFiltrados = new DataView(datosExcel);
+                dataGridView1.DataSource = datosFiltrados;
+
+                // Llenar combo municipios
+                if (datosExcel.Columns.Contains("Municipio"))
+                {
+                    cmbBoxMunicipio.Items.Clear();
+                    cmbBoxMunicipio.Items.Add("Ninguno");
+                    foreach (var municipio in datosExcel.AsEnumerable()
+                        .Select(r => r.Field<string>("Municipio"))
+                        .Where(m => !string.IsNullOrEmpty(m))
+                        .Distinct()
+                        .OrderBy(m => m))
+                    {
+                        cmbBoxMunicipio.Items.Add(municipio);
+                    }
+                    cmbBoxMunicipio.SelectedIndex = 0;
+                }
+
+                txtEstado.Text = datosExcel.Columns.Contains("Entidad") && datosExcel.Rows.Count > 0
+                    ? datosExcel.Rows[0]["Entidad"].ToString()
+                    : "Sin datos";
+
+                txtNumeroAfiliaciones.Text = datosExcel.Rows.Count.ToString();
+
+                if (datosExcel.Columns.Contains("FechaAfiliacion") && datosExcel.Rows.Count > 0)
+                {
+                    DateTime minFecha = DateTime.MaxValue;
+                    DateTime maxFecha = DateTime.MinValue;
+                    foreach (DataRow row in datosExcel.Rows)
+                    {
+                        var valor = row["FechaAfiliacion"];
+                        // Verifica el tipo y valor aquí si lo necesitas
+                    }
+                    if (minFecha != DateTime.MaxValue && maxFecha != DateTime.MinValue)
+                    {
+                        dtpFechaInicio.Value = minFecha;
+                        dtpFechaFin.Value = maxFecha;
+                        dtpFechaInicio.MinDate = minFecha;
+                        dtpFechaFin.MinDate = minFecha;
+                        dtpFechaInicio.MaxDate = maxFecha;
+                        dtpFechaFin.MaxDate = maxFecha;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar Excel: " + ex.Message);
+            }
+            finally
+            {
+                btnCargar.Enabled = true;
+                btnCargar.Text = "Cargar Excel";
+            }
+        }
+
+        // Método central para filtrar por municipio y fechas
+        private void AplicarFiltros()
+        {
+            if (datosFiltrados == null) return;
+
+            string filtro = "";
+
+            // Filtro por municipio
+            if (cmbBoxMunicipio.SelectedItem != null && cmbBoxMunicipio.SelectedItem.ToString() != "Ninguno")
+            {
+                filtro = $"MUNICIPIO = '{cmbBoxMunicipio.SelectedItem.ToString().Replace("'", "''")}'";
+            }
+
+            // Filtro por fecha
+            if (checkBoxFecha.Checked && datosExcel.Columns.Contains("FECHA_AFILIACION"))
+            {
+                DateTime inicio = dtpFechaInicio.Value;
+                DateTime fin = dtpFechaFin.Value;
+
+                // Solo filtra si el rango es válido
+                if (inicio <= fin)
+                {
+                    string filtroFecha = $"FECHA_AFILIACION >= #{inicio:MM/dd/yyyy}# AND FECHA_AFILIACION <= #{fin:MM/dd/yyyy}#";
+                    if (filtro != "") filtro += " AND ";
+                    filtro += filtroFecha;
+                }
+            }
+
+            datosFiltrados.RowFilter = filtro;
+            dataGridView1.DataSource = datosFiltrados;
+            txtNumeroAfiliaciones.Text = datosFiltrados.Count.ToString();
+        }
+
+        private void BtnResetear_Click(object sender, EventArgs e)
+        {
+            txtNombreArchivo.Text = "";
+            txtEstado.Text = "";
+            txtNumeroAfiliaciones.Text = "0";
+            cmbBoxMunicipio.Items.Clear();
+            cmbBoxMunicipio.Items.Add("Ninguno");
+            cmbBoxMunicipio.SelectedIndex = 0;
+            dataGridView1.DataSource = null;
+            datosExcel.Clear();
+            datosExcel.Columns.Clear();
+            datosFiltrados = null;
+            checkBoxFecha.Checked = false;
+            dtpFechaInicio.Enabled = false;
+            dtpFechaFin.Enabled = false;
+            dtpFechaInicio.Value = DateTime.Today;
+            dtpFechaFin.Value = DateTime.Today;
+        }
+
+        
+        // Evento cuando cambia la fecha de inicio
+        private void dtpFechaInicio_ValueChanged(object sender, EventArgs e)
+        {
+            AplicarFiltros();
+        }
+
+        // Evento cuando se marca/desmarca el filtro de fecha
+        private void checkBoxFecha_CheckedChanged(object sender, EventArgs e)
+        {
+            dtpFechaInicio.Enabled = checkBoxFecha.Checked; // Habilita/deshabilita el campo
+            dtpFechaFin.Enabled = checkBoxFecha.Checked;
+            AplicarFiltros();
+        }
+
+        private void cmbBoxMunicipio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AplicarFiltros();
+        }
+
+        private void dtpFechaFin_ValueChanged_1(object sender, EventArgs e)
+        {
+            AplicarFiltros();
+        }
+    }
+}
